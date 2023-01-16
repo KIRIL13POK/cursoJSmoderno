d286501917c0473b48d112567c0ad411abfbd344
Kiril Pokrovskiy <p1kr1vskiy@gmail.com>
2023-01-16 11:46:43 +0100

    Leer los datos del curso que seleccionamos
    -->LeerDatosCurso(curso)

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -20,6 +20,20 @@ function cargarEventListeners(){
 function agregarCurso(e){
     e.preventDefault();//prevenimos la acción por default
     if(e.target.classList.contains('agregar-carrito')){
-        console.log('Agregando al carrito.....')
+        const cursoSeleccionado = e.target.parentElement.parentElement; 
+        leerDatosCurso(cursoSeleccionado);  
     }; 
+};
+//Lee el contenido de HTML al que le dimos el click y extrae la información del curso
+function leerDatosCurso(curso){
+    console.log(curso);
+    //Crear un objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad:1,
+    }
+    console.log(infoCurso);
 };
