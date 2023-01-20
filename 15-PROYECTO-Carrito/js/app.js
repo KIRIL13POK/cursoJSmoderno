09ba4e92200f75dc2be03e773834829f0808918d
Kiril Pokrovskiy <p1kr1vskiy@gmail.com>
2023-01-20 19:48:46 +0100

    Mostrando en el Carrito el curso seleccionado

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -7,33 +7,72 @@ const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 // console.log(contenedorCarrito);
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 // console.log(vaciarCarritoBtn);
+let articulosCarrito = [];
+
 
 
 cargarEventListeners();
-function cargarEventListeners(){
+function cargarEventListeners() {
     //Cuando agregas un curso presionando "Agregar al carrito"
     listaCursos.addEventListener('click', agregarCurso);
 };
 
 //Funciones
 // Función que añade el curso al carrito
-function agregarCurso(e){
+function agregarCurso(e) {
     e.preventDefault();//prevenimos la acción por default
-    if(e.target.classList.contains('agregar-carrito')){
-        const cursoSeleccionado = e.target.parentElement.parentElement; 
-        leerDatosCurso(cursoSeleccionado);  
-    }; 
+    if (e.target.classList.contains('agregar-carrito')) {
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
+    };
 };
 //Lee el contenido de HTML al que le dimos el click y extrae la información del curso
-function leerDatosCurso(curso){
-    console.log(curso);
+function leerDatosCurso(curso) {
+    //console.log(curso);
     //Crear un objeto con el contenido del curso actual
     const infoCurso = {
         imagen: curso.querySelector('img').src,
         titulo: curso.querySelector('h4').textContent,
         precio: curso.querySelector('.precio span').textContent,
         id: curso.querySelector('a').getAttribute('data-id'),
-        cantidad:1,
+        cantidad: 1,
     }
-    console.log(infoCurso);
+    // Agregar elementos al array de carrito
+    articulosCarrito = [...articulosCarrito, infoCurso];
+
+    console.log(articulosCarrito);
+
+    carritoHTML();
+
 };
+
+//Muestra el Carrito de compras en el HTML
+function carritoHTML(){
+
+    //Limpiar el HTML
+
+    limpiarHTML();
+
+    //Recorre el carrito y genera el HTML
+    articulosCarrito.forEach( curso => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>
+        ${curso.titulo}
+        </td>
+        `
+        //Agrega el HTML en el carrito de tbody
+        contenedorCarrito.appendChild(row);
+    });
+}
+
+// Elemina los cursos del tbody
+function limpiarHTML(){
+    // Forma lenta
+    // contenedorCarrito.innerHTML = '';
+
+    while(contenedorCarrito.firstChild){
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+    }
+}
+
