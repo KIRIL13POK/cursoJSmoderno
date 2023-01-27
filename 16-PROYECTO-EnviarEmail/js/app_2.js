f9fe09420d4522b1b3307efe4cdc452826ff5312
Kiril Pokrovskiy <p1kr1vskiy@gmail.com>
2023-01-27 12:32:12 +0100

    . Prevenir que se generen múltiples alertas
    --> //1- le estamos diciendo que únicamente en los elementos de este div busque esta clase ---const alerta = referencia.querySelector('.bg-red-600');---
    --/2-no me genera múltiples alertas y entonces lo elimine  ---if(alerta){
                alerta.remove();---

--- 16-PROYECTO-EnviarEmail/js/app_2.js
+++ 16-PROYECTO-EnviarEmail/js/app_2.js
@@ -12,29 +12,34 @@ document.addEventListener('DOMContentLoaded', function () {
     textAreaMensaje.addEventListener('blur', validar);
 
     function validar(evt) {
-        console.log(evt.target.parentElement);//2-referencia al elemento padre.--> el div que contiene ese input y que es su elemento padre.
-        /*
-        console.log(evt.target.parentElement.nextElementSibling);//ir al siguiente elemento-->con estas funciones puedes ir recorriendo todo tu DOM y acceder a un elemento en específico.
-        */
+        console.log(evt.target.parentElement);
 
 
         const tipoCampo = evt.target.id
         if (evt.target.value.trim() === '') {
-            mostrarAlerta(`El campo ${tipoCampo.toUpperCase()} es obligatorio`, evt.target.parentElement);//3-Entonces aquí estamos pasando la referencia del div que contiene tanto el label como el input.
+            mostrarAlerta(`El campo ${tipoCampo.toUpperCase()} es obligatorio`, evt.target.parentElement);
         } else {
             console.log('hay algo');
         }
 
     }
 
-    function mostrarAlerta(mensaje, referencia) {// 1-tomar un segundo parámetro, que sería el elemento en el cual se va a añadir esta alerta.
-        // Generar alerta en HTML
+    function mostrarAlerta(mensaje, referencia) {
+        //Compruenba si ya existe una alerta
+        const alerta = referencia.querySelector('.bg-red-600');//1- le estamos diciendo que únicamente en los elementos de este div busque essta clase
+        if(alerta){
+            alerta.remove();
+        }//2-no me genera múltiples alertas-->entonces lo elimine
+
+
+
+        //Generar alerta en HTML
         const error = document.createElement('P');
         error.textContent = mensaje
-        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');// para agregarle clases 
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
 
         //Inyectar el error al formulario
-        referencia.appendChild(error);//4- en lugar de que sea formulario va a ser esa referencia.
+        referencia.appendChild(error);
 
 
     }
