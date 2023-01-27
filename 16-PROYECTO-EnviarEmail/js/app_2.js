d866c36fe1d6e03a527d3d7473367e140f5bdbe1
Kiril Pokrovskiy <p1kr1vskiy@gmail.com>
2023-01-27 11:46:11 +0100

    Mostrando las alertas junto a sus campos
    -->  1-tomar un segundo parámetro, que sería el elemento en el cual se va a añadir esta alerta.
    --> 2-referencia al elemento padre. El div que contiene ese input y que es su elemento padre.
    --- evt.target.parentElement---
    -->  /3-Entonces aquí estamos pasando la referencia del div que contiene tanto el label como el input.
    --- (`El campo ${tipoCampo.toUpperCase()} es obligatorio`, evt.target.parentElement) ---
    --> //4- en lugar de que sea formulario va a ser esa referencia.
    --- referencia.appendChild(error) --

--- 16-PROYECTO-EnviarEmail/js/app_2.js
+++ 16-PROYECTO-EnviarEmail/js/app_2.js
@@ -1,39 +1,42 @@
-document.addEventListener('DOMContentLoaded', function(){
-    
-    
+document.addEventListener('DOMContentLoaded', function () {
+
+
     const inputEmail = document.querySelector('#email'),
-          inputAsunto = document.querySelector('#asunto'),
-          textAreaMensaje = document.querySelector('#mensaje'),
-          formulario = document.querySelector('#formulario');
+        inputAsunto = document.querySelector('#asunto'),
+        textAreaMensaje = document.querySelector('#mensaje'),
+        formulario = document.querySelector('#formulario');
 
     //Asignar eventos
     inputEmail.addEventListener('blur', validar);
     inputAsunto.addEventListener('blur', validar);
-    textAreaMensaje.addEventListener('blur',validar);
+    textAreaMensaje.addEventListener('blur', validar);
 
-    // -->Pero para hacer nuestro código más reutilizable, podemos reemplazar un callback por una función. //Tenemos una sola función que se va a encargar de ello en lugar de tener código repetitivo
-    
-    function validar(evt){
+    function validar(evt) {
+        console.log(evt.target.parentElement);//2-referencia al elemento padre.--> el div que contiene ese input y que es su elemento padre.
+        /*
+        console.log(evt.target.parentElement.nextElementSibling);//ir al siguiente elemento-->con estas funciones puedes ir recorriendo todo tu DOM y acceder a un elemento en específico.
+        */
+
+
         const tipoCampo = evt.target.id
-        //console.log(evt.target.id)//para  encontrar información sobre el elemento que dispara ese evento.
-        if(evt.target.value.trim() === ''){
-            mostrarAlerta(`El campo ${tipoCampo.toUpperCase ()} es obligatorio`);// de esa forma se va a inyectar dinámicamente el ID de cada uno de los campos   
-        }else{
+        if (evt.target.value.trim() === '') {
+            mostrarAlerta(`El campo ${tipoCampo.toUpperCase()} es obligatorio`, evt.target.parentElement);//3-Entonces aquí estamos pasando la referencia del div que contiene tanto el label como el input.
+        } else {
             console.log('hay algo');
         }
-        
-    }
-   
-    function mostrarAlerta(mensaje){
-       // Generar alerta en HTML
-       const error = document.createElement('P');
-       error.textContent = mensaje
-       error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');// para agregarle clases 
 
-       //Inyectar el error al formulario
-       formulario.appendChild(error);//Agregar un nuevo elemento a lo ya existente.
-      
-       
+    }
+
+    function mostrarAlerta(mensaje, referencia) {// 1-tomar un segundo parámetro, que sería el elemento en el cual se va a añadir esta alerta.
+        // Generar alerta en HTML
+        const error = document.createElement('P');
+        error.textContent = mensaje
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');// para agregarle clases 
+
+        //Inyectar el error al formulario
+        referencia.appendChild(error);//4- en lugar de que sea formulario va a ser esa referencia.
+
+
     }
 
 
