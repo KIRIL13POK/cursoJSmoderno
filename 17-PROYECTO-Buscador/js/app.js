7ecb9f6a5c1e85f2f3c8bb2f1229e5fef2c6136b
Kiril Pokrovskiy <p1kr1vskiy@gmail.com>
2023-02-03 19:52:05 +0100

    Limpio preparado para  Leer el valor de la marca del automovil

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -1,27 +1,16 @@
 //VARIABLES
 const resultado = document.querySelector('#resultado');
-console.log(resultado);
+const year = document.querySelector('#year');
 
-const year = document.querySelector('#year')
-console.log(year);
-
-
-//agencia de automóviles no quiere vender automóviles de más de diez años.--> utilizar estos dos valores para generar las diferentes opciones 
 const max = new Date().getFullYear();
 const min = max- 10;
-//La ventaja es que este getFullerYear cuando se presenta el cambio de año, va a tomar el año actual en el cual estés y no vas a tener que venir a cambiarlo manualmente.
-
-
 
 //Eventos
 document.addEventListener('DOMContentLoaded', () => {
-    mostrarAutos();//muestra los automobiles al cargar
-
-
+    //Muestra los automobiles al cargar
+    mostrarAutos();
     //Llena las opciones del año
     llenarSelect();
-
-
 
 });
 
@@ -37,7 +26,7 @@ function mostrarAutos() {
         `;
         // insertar en elHTML
         resultado.appendChild(autoHTML);
-    })
+    });
 
 }
 
@@ -45,10 +34,10 @@ function mostrarAutos() {
 function llenarSelect(){
    
     for (let i = max ; i >= min ; i--){
-        const opcion = document.createElement('option');// creamos elemento
-         opcion.value = i;// value es un metodo que tiene input y le asignamos  valor i
-         opcion.textContent = i;//
-         year.appendChild(opcion);//Agrega opcion de año
+        const opcion = document.createElement('option');
+         opcion.value = i;
+         opcion.textContent = i;
+         year.appendChild(opcion);
 
     }
 }
