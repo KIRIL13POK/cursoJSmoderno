da943f5e0498042d0f7806e113faadd498d7e121
Kiril Pokrovskiy <p1kr1vskiy@gmail.com>
2023-02-04 01:01:41 +0100

    Leer el valor de la marca del automovil

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -1,9 +1,32 @@
 //VARIABLES
-const resultado = document.querySelector('#resultado');
+const marca = document.querySelector('#marca');
 const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+
+//Contenedor para los resultados
+const resultado = document.querySelector('#resultado');
+
 
 const max = new Date().getFullYear();
-const min = max- 10;
+const min = max - 10;
+
+//Generar  un objeto con la busqueda
+const datosBusqueda = {
+    marca: '',
+    year:'',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transmision: '',
+    color : '',
+
+}
+
 
 //Eventos
 document.addEventListener('DOMContentLoaded', () => {
@@ -13,6 +36,32 @@ document.addEventListener('DOMContentLoaded', () => {
     llenarSelect();
 
 });
+
+//Event listener para select de búsqueda
+marca.addEventListener('change',e => {
+    datosBusqueda.marca = e.target.value;
+});
+year.addEventListener('change',e =>{
+    datosBusqueda.year = e.target.value;
+});
+minimo.addEventListener('change', e =>{
+    datosBusqueda.minimo= e.target.value;
+});
+maximo.addEventListener('change', e =>{
+    datosBusqueda.maximo= e.target.value;
+});
+puertas.addEventListener('change', e =>{
+    datosBusqueda.puertas= e.target.value;
+});
+transmision.addEventListener('change', e =>{
+    datosBusqueda.transmision= e.target.value;
+});
+color.addEventListener('change', e =>{
+    datosBusqueda.color= e.target.value;
+    console.log(datosBusqueda);
+});
+
+
 
 function mostrarAutos() {
     autos.forEach(auto => {
@@ -31,13 +80,13 @@ function mostrarAutos() {
 }
 
 //Genera años de select
-function llenarSelect(){
-   
-    for (let i = max ; i >= min ; i--){
+function llenarSelect() {
+
+    for (let i = max; i >= min; i--) {
         const opcion = document.createElement('option');
-         opcion.value = i;
-         opcion.textContent = i;
-         year.appendChild(opcion);
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild(opcion);
 
     }
 }
