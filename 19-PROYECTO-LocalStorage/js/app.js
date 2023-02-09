9a470faa0cf54155503c7c78097c7109bbdd8be3
Kiril Pokrovskiy <p1kr1vskiy@gmail.com>
2023-02-09 18:12:41 +0100

    Validando el Formulario

--- 19-PROYECTO-LocalStorage/js/app.js
+++ 19-PROYECTO-LocalStorage/js/app.js
@@ -18,5 +18,34 @@ function eventEventListeners(){
 function agregarTweet(e){
     e.preventDefault();
 
-    console.log('Agregando tweet');
+    //Textarea donde el usuario escribe
+    const tweet = document.querySelector('#tweet').value;
+
+    //Validacion
+    if(tweet ===''){
+        mostrarError('No puede ir vacio');
+        return;// evita de se ejecuta más lineas de codigo
+
+    }
+
+    console.log('agregando tweet');
+    console.log(tweet);
+}
+
+//Mostrar mensaje de error
+function mostrarError(error) {
+    const mensajeError = document.createElement('P');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    //Insertarlo en el contenido
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+    //Elimina la alerta despues de 3 segundos
+    setTimeout(() => {
+        mensajeError.remove();
+
+    }, 3000);
+
 }
