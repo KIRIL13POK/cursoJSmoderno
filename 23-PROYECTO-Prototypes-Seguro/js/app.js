abe238b1f6f3581b1dd5c0675d96a29d0503b947
Kiril Pokrovskiy <p1kr1vskiy@gmail.com>
2023-02-13 22:34:03 +0100

    Crear un Prototype para mostrar un error

--- 23-PROYECTO-Prototypes-Seguro/js/app.js
+++ 23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -9,8 +9,8 @@ function Seguro (marca,year, tipo){
 }
 
 function UI() {}
-console.log(UI)
 
+//Prtotype
 //Llena las opciones de los años
 UI.prototype.llenarOpciones = () => {
     const max = new Date().getFullYear(),
@@ -26,13 +26,39 @@ UI.prototype.llenarOpciones = () => {
     }
 }
 
+//Muesta alertas en pantalla
+UI.prototype.mostrarMensaje = (mensaje, tipo) => {
+    const div = document.createElement('DIV');
+
+    if(tipo === 'error'){
+        div.classList.add('error');  
+    }else{
+        div.classList.add('correcto');  
+
+    }
+
+    div.classList.add('mensaje','mt-10'); 
+    div.textContent = mensaje;
+
+    //Insertar HTML
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.insertBefore(div, document.querySelector('#resultado'));//-->insertBefore() es un método de JavaScript que se utiliza para insertar un nodo como hijo de otro nodo en el árbol de nodos del Document Object Model (DOM). Este método toma dos argumentos: el nodo a ser insertado (el "nodo nuevo") y el nodo referencia (el "nodo hermano"). El nodo nuevo se insertará antes del nodo hermano en el árbol de nodos del DOM.
+
+    // Para limpiar ese mensaje de error ==>
+    setTimeout( () => {
+        div.remove();
+    },3000);
+
+}
+
+
 //Instaciar UI
 
 const ui = new UI();
 
 document.addEventListener('DOMContentLoaded',() => {
     ui.llenarOpciones();//Llenar el select con años
-
+    
 })
 
 eventListeners();
@@ -54,9 +80,16 @@ function cotizarSeguro(e){
     const tipo = document.querySelector('input[name="tipo"]:checked').value;
 
     if(marca === '' || year === '' || tipo === ''){
-        
-    }else{
-        
+        ui.mostrarMensaje('Todos los campos son obligatirios','error')
+        return; 
+        // Si alguno de estos campos está vacío, se muestra un mensaje de error utilizando el método mostrarMensaje de la clase "ui". El mensaje se pasa como argumento a mostrarMensaje y su segundo argumento es una cadena "error" que indica que se trata de un mensaje de error.
     }
+    
+    ui.mostrarMensaje('Todo Correcto','correcto');
+    // Si todos los campos están llenos, se muestra un mensaje de éxito con el método mostrarMensaje de la clase "ui". El mensaje se pasa como argumento y su segundo argumento es una cadena "correcto" que indica que se trata de un mensaje de éxito.
+
+
+
+
     
 }
