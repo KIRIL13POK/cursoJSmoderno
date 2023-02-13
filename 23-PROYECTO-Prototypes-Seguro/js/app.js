dfb7d5d7495252194b16405cd7470e6c1e70a893
Kiril Pokrovskiy <p1kr1vskiy@gmail.com>
2023-02-13 21:56:13 +0100

    Validar el Formulario

--- 23-PROYECTO-Prototypes-Seguro/js/app.js
+++ 23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -35,3 +35,29 @@ document.addEventListener('DOMContentLoaded',() => {
 
 })
 
+eventListeners();
+function eventListeners(){
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.addEventListener('submit',cotizarSeguro);//-->Cuando se envía el formulario, se ejecutará la función cotizarSeguro
+}
+
+function cotizarSeguro(e){
+    e.preventDefault();//--> es un método que detiene el comportamiento por defecto de un evento. Por ejemplo, si un formulario se envía con un evento de submit, el navegador normalmente recargará la página y enviará los datos del formulario al servidor. Si se llama a preventDefault en el controlador de eventos del submit, se evitará la acción por defecto y los datos del formulario no se enviarán.
+
+    //Leer la marca slecionada
+    const marca = document.querySelector('#marca').value;
+
+    //Leer el año slecionado
+    const year = document.querySelector('#year').value;
+
+    //Leer el tipo de cobertura
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+   //Este es un bloque if que verifica si alguna de las variables marca, year o tipo está vacía -->
+    if(marca === '' || year === '' || tipo === ''){
+        console.log('No paso la validacion');
+    }else{
+        console.log('Exelente, todo correcto');
+    }
+    
+}
