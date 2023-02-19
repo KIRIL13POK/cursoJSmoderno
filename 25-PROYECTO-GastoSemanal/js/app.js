b8531d90a4245002736f77eeafa03bda9fb67c01
Kiril Pokrovskiy <p1kr1vskiy@gmail.com>
2023-02-20 00:59:37 +0100

    Primeros pasos con el Proyecto
    --> window.location.reload();//hace recargar la ventana actual

---
+++ 25-PROYECTO-GastoSemanal/js/app.js
@@ -0,0 +1,23 @@
+//Variables Y selectores
+const formulario = document.querySelector('#agregar-gasto');
+const gastoListado = document.querySelector('#gastos ul');
+
+//Eventos
+
+eventListeners();
+function eventListeners(){
+    document.addEventListener('DOMContentLoaded',preguntarPresupuesto)
+}
+
+//Clases
+
+
+//Funciones
+
+function preguntarPresupuesto(){
+    const presupuestoUsuario = prompt('Cual es tu presupuesto?');
+    console.log(presupuestoUsuario);
+    if(presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) ||presupuestoUsuario <=0 ){
+        window.location.reload();//hace recargar la ventana actual
+    }
+}
